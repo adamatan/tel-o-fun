116f68c7b573661c663eff34ebb9fd803ef9dde1
Adam Matan <adam@matan.name>
2017-07-19 10:11:12 +0300

    Working version with remote URL

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const xray = require('x-ray');
+const Xray = require('x-ray');
+
+const xray = Xray();
+
+const url = 'https://www.tel-o-fun.ga/';
+
+xray(url, '.marker', [['.title']])((err, value) => {
+  console.log(value);
+});
+
