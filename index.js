c2920b816de384871c9c35a4663f0b95ac106e8c
Adam Matan <adam@matan.name>
2017-07-21 06:34:04 +0300

    Parse the stationId as number

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+const path = require('path');
 const Xray = require('x-ray');
+const read = require('fs').readFileSync;
 
+const html = read(path.resolve(__dirname, 'index.html'));
 const xray = Xray();
 
-const url = 'https://www.tel-o-fun.ga/';
-
-xray(url, '.marker', [['.title']])((err, value) => {
-  console.log(value);
+xray(html, '.marker', [
+  {
+    bikes: '.bikes',
+    lat: '@data-lat',
+    lon: '@data-lng',
+    address: '@data-name',
+    stationId: '@data-id' },
+])((err, stations) => {
+  const processedStations = stations.map(s =>
+    Object.assign({}, s, { stationId: parseInt(s.stationId, 10) }));
+  console.log(processedStations.slice(0, 2));
 });
 
