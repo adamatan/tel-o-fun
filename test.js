76ea0c2ca55ec097b660a3711c8941d25dc855db
Adam Matan <adam@matan.name>
2017-07-23 18:04:38 +0300

    Added new test

--- test.js
+++ test.js
@@ -6,7 +6,19 @@ const telofun = require('.');
 
 const html = read(path.resolve(__dirname, 'examples/local/index.html'));
 
-test('Station Count', t => telofun(html)
-  .then((result) => {
-    t.is(result.length > 200, true);
-  }));
+let stations;
+
+test.before(async () => {
+  stations = await telofun(html);
+});
+
+test('Station Count', (t) => {
+  t.is(stations.length > 200, true);
+});
+
+test('Every station object has all expected fields', (t) => {
+  t.is(stations.every((station) => {
+    return ('stationId' in station);
+  }), true);
+});
+
