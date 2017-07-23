65a24160ab4919ac8a1d7d31974f4e7d557b4a1d
Adam Matan <adam@matan.name>
2017-07-23 17:34:39 +0300

    Lint

--- test.js
+++ test.js
@@ -7,7 +7,9 @@ const telofun = require('.');
 const html = read(path.resolve(__dirname, 'examples/local/index.html'));
 
 test('Station Count', (t) => {
-  return telofun(html).then((result) => {
+  return telofun(html)
+  .then((result) => {
     t.is(result.length > 200, true);
   });
-});
+},
+);
