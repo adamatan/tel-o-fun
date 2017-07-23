a8cb2560211844f06e4c9a8b6360f3bd7d2a1b92
Adam Matan <adam@matan.name>
2017-07-23 17:33:09 +0300

    Remove dummy test

--- test.js
+++ test.js
@@ -1,16 +1,13 @@
 import test from 'ava';
-const assert = require('assert');
+
 const path = require('path');
 const read = require('fs').readFileSync;
 const telofun = require('.');
+
 const html = read(path.resolve(__dirname, 'examples/local/index.html'));
 
-test('foo', (t) => {
-  t.pass();
-});
-
-test('Station Count', (t) => ({
-  telofun(html).then((result) => {
+test('Station Count', (t) => {
+  return telofun(html).then((result) => {
     t.is(result.length > 200, true);
   });
-}));
+});
